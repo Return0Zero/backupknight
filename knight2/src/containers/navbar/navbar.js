52f4736769558f4fd79c6def9140cc67eaf52c58
Return0Zero <69299788+Return0Zero@users.noreply.github.com>
2022-07-27 07:39:37 -0400

    Update navbar.js

--- knight2/src/containers/navbar/navbar.js
+++ knight2/src/containers/navbar/navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
     <div className="navbar-max-width">
       <div className="navbar-container">
         <a href='#servicesID' className='link-services'>Services</a>
-        <a href="https://www.http://knightmultiservice.com/.com" className='link--title-logo'><img src={LogoHorizontal} alt='logo' className='logo'/></a>
+        <a href="http://knightmultiservice.com/" className='link--title-logo'><img src={LogoHorizontal} alt='logo' className='logo'/></a>
         <a href='#contactID' className='link-contact'>Contact Us</a>
       </div>
       <NavbarMobile className='navbar-mobile'></NavbarMobile>
